0f8d937a916aca08c5465c5f45a046277f136c8f
marcosaciv <marcosaciveiro@hotmail.com>
2017-01-16 12:20:24 +0100

    Codigo para boton

--- app/src/main/java/com/dam216/gitp/MainActivity.java
+++ app/src/main/java/com/dam216/gitp/MainActivity.java
@@ -2,6 +2,8 @@ package com.dam216.gitp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view)
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
